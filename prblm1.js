9bce0a7eddcabec38e2ac7bc3540739adb669270
yashkhare11 <yash.khare2525@gmail.com>
2022-09-13 22:55:39 +0530

    Code Fixed

--- prblm1.js
+++ prblm1.js
@@ -1,9 +1,10 @@
 // Check whether a number is Prime or not ?
+
 let num=4;
-for(i=2;i<=num;i++){
+for(i=2;i<num;i++){
   if(num%i==0){
-    console.log("is prime")
+    console.log("is not prime")
   }
-  else{console.log("Not Prime")}
+  else{console.log(" Is Prime")}
   
 }
